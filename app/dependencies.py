f3d5577f0c6acdb26a6a7274129e9fb443c4bcb0
Cherinet Hailu <cherinethailu619@gmail.com>
2025-03-02 09:35:56 +0300

    Optimize imports

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,7 +1,4 @@
-from sqlalchemy.orm import Session
 from .database import SessionLocal
-from .config import SECRET_KEY, ACCESS_TOKEN_EXPIRE_MINUTES
-
 # Dependency to get database session
 def get_db():
     db = SessionLocal()
